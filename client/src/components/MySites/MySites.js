5eddd7ac4f9e52e57c72c477f423764b49179cee
Lluis Garcia <llugarciapons@gmail.com>
2021-09-15 22:03:09 +0200

    Site created. Edit quantity and delete coin needed

--- client/src/components/MySites/MySites.js
+++ client/src/components/MySites/MySites.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 // import { API_BASE_URL } from "../../constants/apiConstants";
-
 
 
 function MySites(props) {
@@ -22,6 +21,11 @@ function MySites(props) {
         props.history.push('/');
     };
 
+    const redirectToSite = (site) => {
+        props.updateTitle(site.name);
+        props.history.push(`/site/${site.id}`);
+    };
+
 
     return (
         <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
@@ -30,14 +34,12 @@ function MySites(props) {
                     <>{sites.map(site => {
                         let src = `//logo.clearbit.com/${site.name.replace(/\s/g, '')}.com?size=48`;
                         return (
-                            <Link key={site.id} to={`/Site/${site.id}`}>
-                                <div className="crypto-parent">
-                                    <div className="crypto-img" ><img src={src} style={{ borderRadius: "50%" }} alt={site.name}></img></div>
-                                    <div className="crypto-info">
-                                        <p className="crypto-info-title">{site.name}</p>
-                                    </div>
+                            <div className="crypto-parent" onClick={() => redirectToSite(site)} key={site.id}>
+                                <div className="crypto-img" ><img src={src} style={{ borderRadius: "50%" }} alt={site.name}></img></div>
+                                <div className="crypto-info">
+                                    <p className="crypto-info-title">{site.name}</p>
                                 </div>
-                            </Link>
+                            </div>
                         );
                     })
                     }</>)
@@ -49,4 +51,4 @@ function MySites(props) {
 
     );
 }
-export default MySites;
+export default withRouter(MySites);
--- client/src/components/Site/Site.js
+++ client/src/components/Site/Site.js
@@ -1,6 +1,75 @@
-function Site(props) {
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams, withRouter } from "react-router-dom";
+import "./Site.css";
 
-    return "Site";
+function Site(props) {
+    const [site, setSite] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [edit, setEdit] = useState(false);
+
+    const { siteId } = useParams();
+
+    useEffect(() => {
+        const getSiteInfo = async () => {
+            const response = await axios.get(`/api/site/find/${siteId}`);
+            console.log(response.data);
+            props.updateTitle(response.data.site.name);
+            setSite(response.data.site);
+            setIsLoading(false);
+        };
+        getSiteInfo();
+    }, []);
+
+    const redirectToHome = () => {
+        props.updateTitle('Wallet');
+        props.history.push('/');
+    };
+
+    return (
+        <div className="card col-12 col-lg-4 login-card mt-2 hv-center">
+            {!isLoading ?
+                (
+                    <>{site?.crypto.map(coin => {
+                        let src = `https://s2.coinmarketcap.com/static/img/coins/32x32/${coin.coinId}.png`;
+                        return (
+                            <div className="crypto-parent-site" key={coin.id}>
+                                <div className="crypto-name">
+                                    <div className="crypto-img" ><img src={src} style={{ borderRadius: "50%" }} alt={coin.symbol}></img></div>
+                                    <div className="crypto-info">
+                                        <p className="crypto-info-title">{coin.symbol}</p>
+                                    </div>
+                                </div>
+
+                                <div className="input-group">
+                                    {/* {coin ? <div className="input-group-prepend">
+                                        <span className="input-group-text bg-transparent  border-0" id="basic-addon1">
+                                            <img src={src} alt="crypto logo" />
+                                        </span>
+                                    </div> : ""} */}
+
+                                    <input
+                                        id="crypto"
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Symbol"
+                                        aria-label="Cryptocurrency"
+                                        aria-describedby="basic-addon2"
+                                        value={coin.quantity}
+                                        disabled={!edit}
+                                    // onChange={(e) => setCoin(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                        );
+                    })
+                    }</>)
+                : "Loading..."}
+            <div className="mt-2">
+                <span className="loginText" onClick={() => redirectToHome()}>Back to Home</span>
+            </div>
+        </div>
+    );
 }
 
-export default Site;
+export default withRouter(Site);
